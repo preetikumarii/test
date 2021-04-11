234ec6602dfe75c5f6bb4e08c1633d46c43ec989
Preeti Kumari <preetikumari93399@gmail.com>
2021-04-11 20:22:51 +0530

    button click on console

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("heyy!!");
+let button = document.querySelector("#btn");
+console.log(button);
+const buttonclick = () =>{
+    console.log("click");
+}
+button.addEventListener("click", buttonclick );
+
